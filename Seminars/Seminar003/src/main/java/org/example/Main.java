0e61abbfb0cf63c238ea9d2df5b77dff9c6abc02
Alexey Shishkarev <Whiteverse@gmail.com>
2023-07-08 22:13:34 +0300

    Solution of task seminar

--- Seminars/Seminar003/src/main/java/org/example/Main.java
+++ Seminars/Seminar003/src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            list1.add(new Random().nextInt(50));
+            list2.add(new Random().nextInt(50));
+        }
+        System.out.println(list1);
+        System.out.println(list2);
+        System.out.println("=".repeat(40));
+        ArrayList<Integer> temp = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            if (list1.get(i) % list2.get(i) == 0) {
+                temp.add(list1.get(i));
+            }
+
+        }
+        list1.removeAll(temp);
+        System.out.println(list1);
     }
 }
